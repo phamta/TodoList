c74bc38156d87aec647ea6a30fe8257469eb5b5d
Tanvan <tanvan.13022014@gmail.com>
2025-03-04 09:48:01 +0700

    user created

---
+++ src/main/java/com/tanvan/todolist/model/User.java
@@ -0,0 +1,9 @@
+package com.tanvan.todolist.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+}
